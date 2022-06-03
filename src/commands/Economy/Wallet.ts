b8ded5eb7b958067dc86fec0f03d0e90db092f98
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-06-03 21:44:47 +0200

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "© Katsushika",
+      footerText: "© kingsley",
       buttons: buttons,
       headerType: 1,
     };
