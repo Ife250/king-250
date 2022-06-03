6bab93feb0c3ea2ecf5a7f33edcbc533bfe48ea5
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-06-04 00:52:05 +0200

    Update Chat.ts

--- src/commands/General/Chat.ts
+++ src/commands/General/Chat.ts
@@ -11,7 +11,7 @@ export default class Command extends BaseCommand {
     super(client, handler, {
       command: "chat",
       description: "Chat with the kingsley Bot in group",
-      aliases: ["bot","kingsley"],
+      aliases: ["bot", "kingsley", "king"],
       category: "fun",
       usage: `${client.config.prefix}bot (text)`,
       baseXp: 30,
