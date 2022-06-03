df909ac526a651fd7c5506170ebccd45fd46b09f
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-06-03 21:51:11 +0200

    Update Chat.ts

--- src/commands/General/Chat.ts
+++ src/commands/General/Chat.ts
@@ -10,8 +10,8 @@ export default class Command extends BaseCommand {
   constructor(client: WAClient, handler: MessageHandler) {
     super(client, handler, {
       command: "chat",
-      description: "Chat with the Bot in group",
-      aliases: ["bot"],
+      description: "Chat with the kingsley Bot in group",
+      aliases: ["bot","kingsley"],
       category: "fun",
       usage: `${client.config.prefix}bot (text)`,
       baseXp: 30,
@@ -36,10 +36,10 @@ export default class Command extends BaseCommand {
           return void M.reply(res.data.cnt);
         })
         .catch(() => {
-          M.reply(`Intriguing...`);
+          M.reply(`legendary kingsley is loading...`);
         });
     } else {
-      M.reply(`Chat Bot Url not set.`);
+      M.reply(`Chat kingsley Bot Url or chill talk not set.`);
     }
   };
 }
